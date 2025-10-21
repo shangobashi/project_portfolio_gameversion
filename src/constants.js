16adf9aa47d06e51b29cbf52d70ab583ea3433dd
shangobashi <eloubw@gmail.com>
2025-10-21 23:36:26 +0200

    finished fixing all the remaining challenges, mainly obstacles definition, dialogue boxes content, and some map polishing. v1 is finished!

--- src/constants.js
+++ src/constants.js
@@ -1,25 +1,31 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  computers_front: `This is my PC. I work mostly in JavaScript/TypeScript. These days, I've been itching towards Vue a lot, but truth be told, I'm still green and curious as hell. I'm giving myself some breathing room to try as much as possible before settling into MY ways.
-    I've made a couple of projects in that JS/TS, React and Python. I also like PostgreSQL a lot, in another domain. And I am looking at Rust in a dangerous way... Anyway regardless of the paradigm, I just love programming.
-    Here is my <a href="https://github.com/shangobashi" target="_blank">Github</a>!`,
+  computers_front: `These are my charts. They are always on. I'm always monitoring the prices of various currencies, commodities and tokens. No one knows when Opportunity is ready to be Stricken!`,
   certificate_fs: `This is my Full-Stack Engineering Professional Certification. I got it from Codecademy. I hung it on the wall because I'm proud of it. It was a very hands-on degree which took me the longest time to begin -- years! -- but once I was in, I was cruising...`,
   certificate_cs: `This is my Computer Sciences Professional Certification. I got it from Codecademy. I hung it on the wall because I'm proud of it. It gave me the solid foundations I needed to tackle any and every other areas I was dying to dive into. To me! The worlds of Computer Vision, Robotics and Blockchain technologies!!`,
-  certificate_aiml: `This is my AI/Machine Learning Engineering Professional Certification. I got it from Codecademy. I hung it on the wall because I'm proud of it. It was a very difficult degree to get around to master; I had to complete it with so many workshops, conferences, lectures, plain interviews, and additional books, to make heads spin.`,
+  certificate_aiml: `This is my AI/Machine Learning Engineering Professional Certification. I got it from Codecademy. I hung it on the wall because I'm proud of it. It was a very difficult degree to get around to master; I had to complete it with so many workshops, conferences, lectures, plain interviews, and additional books -- enough to make heads spin!`,
   table: `That's my Quiet Space. I like to relax here when I can. 
   This is usually where I scroll through my Twitter/X timeline and spread the little knowledge I've managed to accumulate throughout the webosphere. Give me a <a href="https://www.x.com/shangobashi" target="_blank">follow</a>! (If you like the content)
   You'll be able to follow along with my latest experiments!`,
-  gameConsole: `That's my Gaming Setup. I've only gotten interested in video games in earnest during my third decade. And boy, did I go deep. I literally played everything that ever came out and that was interesting at least once, since then.
+  gameConsole: `That's my Gaming Setup (a mini-gaming pc, the best to marry play and work!). I've only gotten interested in video games in earnest during my third decade. And boy, did I go deep. I literally played everything that ever came out and that was interesting at least once, since then.
   And truth be told, I'm not stopping there. Right now, I do have to admit that I have a few favorites that I might have finished for some, or am still playing for most, or am totally stuck at for a few. These includes mainly: Cyberpunk 2077, Black Myth Wukong, Final Fantasy VII, Elden Ring, Clair Obscur 33, and a few others.`,
   bed: `This where I sleep. Sometimes. Most times, I sleep in front of my computers. If I'm not programming, or learning to program better, I am watching the cryptomarkets, scalping like a wild trader as if my life depended on it. I'm a thrill-addicted degen at heart.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/shangobashi/project_portfolio_gameVersion/blob/main/public/resume.pdf" target="_blank">Check it out!</a>
   & DO Contact me at shangobashi@gmail.com if you have any interesting job opportunities!`,
-  computer_right: `Info about this portfolio : It's made with the Kaplay.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  computer_right: `This is my PC. I work mostly in JavaScript/TypeScript. These days, I've been itching towards Vue a lot, but truth be told, I'm still green and curious as hell. I'm giving myself some breathing room to try as much as possible before settling into MY ways.
+    I've made a couple of projects in that JS/TS, React and Python. I also like PostgreSQL a lot, in another domain. And I am looking at Rust in a dangerous way... Anyway regardless of the paradigm, I just love programming.
+    Here is my <a href="https://github.com/shangobashi" target="_blank">Github</a>!`,
   computer_left: `Info about this portfolio : It's made with the Kaplay.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. If you'd like to see a more "regular" version of my portfolio, head over <a href="https://www.shangobashi.duckdns.org" target="_blank">here</a>!`,
   libraries: `There are a lot of books on my shelves. I have books on literally every walls of my dev cell. Programming books, but also books on Finance, Law, Geopolitics, AI, Computer Chips, Electronics. Books on Astronomy, Astrology, Biology, Physics and Mathematics. Fiction by American Authors, Japanese, Chinese, Russian, French, and all accross Africa. Books about writing books. I simply love books...
   I read at least 5 books in a month. My record is 1 book a day, since I'm an adult. Way back when I was a child and had only free time to waste, I'd read between 2-5 books in a single day. I miss those times.`,
+  booksOfMonth: `The books that I am reading this month are: 
+  "The Thinking Machine" by Stephen Witt,
+  "The Coming Wave" by Mustafa Suleyman,
+  "The Code Book -- The Secret History of Codes & Code-Breaking" by Simon Singh,
+  "Le Mage du Kremlin" by Giuliano Da Empoli (I do read/speak French) and
+  "Accelerando" by Charles Stross.`,
+  movies: `This is where I relax and watch my movies. Once upon a time, I applied to Film School. The exam preparation was intense. I had to watch more than 500 movies. I watched them all. And even though I did not get into Film School, I never stopped watching them. With intensity.`,
   exit: `If you want to exit Shango's portfolio, just close the tab.`,
 };
